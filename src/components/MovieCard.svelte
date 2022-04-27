51ebca6a175f83a70938980d103cae3bf29ad31d
NaderNasr <nadernasr7@gmail.com>
2022-04-27 16:09:11 -0400

    create popular and movieCard components

---
+++ src/components/MovieCard.svelte
@@ -0,0 +1,20 @@
+
+<script>
+  // @ts-nocheck
+  export /**
+* @type {{ id: any; }}
+*/
+   let movie;
+</script>
+
+
+<div class="movie-card">
+  <img alt={movie.title} src={`https://image.tmdb.org/t/p/w500` + movie.poster_path}/>
+</div>
+<div class="descr">
+  <h2>{movie.title}</h2>
+  <p>{movie.release_date}</p>
+</div>
+
+
+<style></style>
---
+++ src/components/Popular.svelte
@@ -0,0 +1,28 @@
+<script>
+  export /**
+* @type {any[]}
+*/
+   let popularMovies;
+  // console.log(popularMovies)
+  import MovieCard from "./MovieCard.svelte";
+</script>
+
+<h1>Popular Movies</h1>
+<div class="popular">
+  <!-- Rendering each movie -->
+  {#each popularMovies as movie}
+    <MovieCard {movie}/>
+  {/each}
+</div>
+
+<style>
+  h1{
+    padding: 0 1rem;
+  }
+  .popular{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-column-gap: 1rem;
+    grid-row-gap: 1rem;
+  }
+</style>
