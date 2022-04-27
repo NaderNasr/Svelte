6f153c215854564e8de7a0abd9bb16a9e8669be9
NaderNasr <nadernasr7@gmail.com>
2022-04-27 17:32:14 -0400

    movie card styling

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -1,20 +1,25 @@
-
 <script>
-  // @ts-nocheck
-  export /**
-* @type {{ id: any; }}
-*/
-   let movie;
+	// @ts-nocheck
+	export /**
+	 * @type {{ id: any; }}
+	 */
+	let movie;
 </script>
 
-
 <div class="movie-card">
-  <img alt={movie.title} src={`https://image.tmdb.org/t/p/w500` + movie.poster_path}/>
-</div>
-<div class="descr">
-  <h2>{movie.title}</h2>
-  <p>{movie.release_date}</p>
+	<img alt={movie.title} src={`https://image.tmdb.org/t/p/w500` + movie.poster_path} />
+	<div class="descr">
+		<h2>{movie.title}</h2>
+		<p>{movie.release_date}</p>
+	</div>
 </div>
 
-
-<style></style>
+<style>
+	img {
+		width: 100%;
+    height: 50vh;
+    object-fit: cover;
+    border-radius: 1rem;
+    margin-bottom: 1rem;
+	}
+</style>
