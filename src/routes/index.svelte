902d6c463fe14c4b266c2e1075d2f1cfe3913912
NaderNasr <nadernasr7@gmail.com>
2022-04-27 16:08:43 -0400

    fetch data from tmdb and add popular component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to Svelte Movie DB</h1>
-<a href="/about">About</a>
+<script context="module">
+	//fetch api request
+	export async function load() {
+		const response = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=${
+        import.meta.env.VITE_API_KEY
+      }`
+		);
+		const data = await response.json();
+		console.log(data);
+		if (response.ok) {
+			return {
+				props: {
+					popular: data.results
+				}
+			};
+		}
+	}
+</script>
+
+<script>
+	import Popular from '../components/Popular.svelte';
+	// exporting popular prop from fetch
+	export /**
+* @type {any}
+*/
+	 let popular;
+	// console.log(popular)
+</script>
+
+<!-- Header -->
+<header>
+	<a href="/about">About</a>
+</header>
+
+<!-- Body  -->
+<body>
+	<Popular popularMovies={popular} />
+</body>
