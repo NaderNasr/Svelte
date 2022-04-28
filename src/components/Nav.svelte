b3ba5e03e686ca6ce519e14bb605c889566aec63
NaderNasr <nadernasr7@gmail.com>
2022-04-28 11:12:19 -0400

    navbar

---
+++ src/components/Nav.svelte
@@ -0,0 +1,25 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/">Popular</a>
+  <a href="/">Comedy</a>
+  <a href="/">Documentary</a>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    min-height: 10vh;
+    align-items: center;
+    justify-content: center;
+    
+  }
+
+  a {
+    font-size: 1rem;
+    font-weight: bold;
+    font-family: 'Limelight';
+    color: black;
+    text-decoration: none;
+    padding: 10px;
+  }
+</style>
