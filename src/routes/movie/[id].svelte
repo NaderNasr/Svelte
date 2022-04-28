eff8c0b1a2296cada5428ab2070cdd5259f20536
NaderNasr <nadernasr7@gmail.com>
2022-04-28 09:29:37 -0400

    fetch id of each movie

---
+++ src/routes/movie/[id].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+	//fetch api request
+	// @ts-ignore
+	export async function load({params}) {
+    console.log('params>>',params.id)
+		const response = await fetch(
+			`https://api.themoviedb.org/3/movie/${params.id}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`
+		);
+		const movieDetail = await response.json();
+		console.log('movieDetails>>',movieDetail);
+		if (response.ok) {
+			return {
+				props: {
+					// @ts-ignore
+					movieDetail
+				}
+			};
+		}
+	}
+</script>
+
+<script>
+  export let movieDetail
+</script>
