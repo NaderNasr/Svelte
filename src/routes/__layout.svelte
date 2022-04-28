aab403dd3a421d685c809576da4742bfe0f9bdbd
NaderNasr <nadernasr7@gmail.com>
2022-04-28 10:44:40 -0400

    adding nav/footer layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import global from '../global.css';
+</script>
+
+<svelte:head>
+	<title>Svelte Movie DB</title>
+</svelte:head>
+
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+	</ul>
+</nav>
+
+<slot />
