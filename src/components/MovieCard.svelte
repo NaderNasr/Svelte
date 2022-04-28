63801a0e0424011a5deb7c0bf8fac936480d466b
NaderNasr <nadernasr7@gmail.com>
2022-04-28 09:28:41 -0400

    redirect each movie to id page

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -7,7 +7,10 @@
 </script>
 
 <div class="movie-card">
-	<img alt={movie.title} src={`https://image.tmdb.org/t/p/w500` + movie.poster_path} />
+  <!-- on image click redirect to movie id -->
+	<a href={"/movie/" + movie.id}>
+		<img alt={movie.title} src={`https://image.tmdb.org/t/p/w500` + movie.poster_path} />
+	</a>
 	<div class="desc">
 		<h2>{movie.title}</h2>
 		<p>{movie.release_date}</p>
@@ -17,25 +20,24 @@
 <style>
 	img {
 		width: 100%;
-    height: 50vh;
-    object-fit: cover;
-    border-radius: 1rem;
-    margin-bottom: 1rem;
+		height: 50vh;
+		object-fit: cover;
+		border-radius: 1rem;
+		margin-bottom: 1rem;
 	}
-  h2 {
-    font-size: 20px;
-  }
-  desc {
-    height: 5vh;
-  }
-  p {
-    font-size: 0.7rem;
-  }
-  .movie-card{
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding: 0.1rem;
-  }
-
+	h2 {
+		font-size: 20px;
+	}
+	desc {
+		height: 5vh;
+	}
+	p {
+		font-size: 0.7rem;
+	}
+	.movie-card {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		padding: 0.1rem;
+	}
 </style>
