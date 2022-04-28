b1a57b04997b5c181a1b13abc85b0f6b4de9ce80
NaderNasr <nadernasr7@gmail.com>
2022-04-27 23:37:55 -0400

    movie card styling

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -8,7 +8,7 @@
 
 <div class="movie-card">
 	<img alt={movie.title} src={`https://image.tmdb.org/t/p/w500` + movie.poster_path} />
-	<div class="descr">
+	<div class="desc">
 		<h2>{movie.title}</h2>
 		<p>{movie.release_date}</p>
 	</div>
@@ -22,4 +22,20 @@
     border-radius: 1rem;
     margin-bottom: 1rem;
 	}
+  h2 {
+    font-size: 20px;
+  }
+  desc {
+    height: 5vh;
+  }
+  p {
+    font-size: 0.7rem;
+  }
+  .movie-card{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 0.1rem;
+  }
+
 </style>
