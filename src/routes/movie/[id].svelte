0c702a0b18ef7cbcfdb2d43fc9659fb2e9f949f9
NaderNasr <nadernasr7@gmail.com>
2022-04-28 10:07:08 -0400

    movie description styling

--- src/routes/movie/[id].svelte
+++ src/routes/movie/[id].svelte
@@ -1,13 +1,15 @@
 <script context="module">
 	//fetch api request
 	// @ts-ignore
-	export async function load({params}) {
-    console.log('params>>',params.id)
+	export async function load({ params }) {
+		console.log('params>>', params.id);
 		const response = await fetch(
-			`https://api.themoviedb.org/3/movie/${params.id}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`
+			`https://api.themoviedb.org/3/movie/${params.id}?api_key=${
+				import.meta.env.VITE_API_KEY
+			}&language=en-US`
 		);
 		const movieDetail = await response.json();
-		console.log('movieDetails>>',movieDetail);
+		console.log('movieDetails>>', movieDetail);
 		if (response.ok) {
 			return {
 				props: {
@@ -20,5 +22,51 @@
 </script>
 
 <script>
-  export let movieDetail
+	// @ts-nocheck
+
+	export /**
+	 * @type {{ backdrop_path: string; }}
+	 */
+	let movieDetail;
 </script>
+
+<div class="movie-details">
+	<img
+		src={`https://image.tmdb.org/t/p/original` + movieDetail.backdrop_path}
+		alt={movieDetail.title}
+	/>
+	<div class="text-container">
+		<h1>{movieDetail.title}</h1>
+		<p class="overview">
+			{movieDetail.overview}
+		</p>
+		<p>
+			<span>Release Date</span>{movieDetail.release_date}<br />
+			<span>Budget</span>{movieDetail.budget}<br />
+			<span>Rating</span>{movieDetail.vote_average}<br />
+			<span>Runtime</span>{movieDetail.runtime}<br />
+		</p>
+	</div>
+</div>
+
+<style>
+	h1 {
+		padding: 1rem 0rem 2rem;
+	}
+
+	p {
+		padding: 1rem 0rem;
+	}
+
+	img {
+		width: 100%;
+		border-radius: 1rem;
+	}
+
+	.movie-details {
+		margin: 2rem 20%;
+	}
+	span {
+		font-weight: bold;
+	}
+</style>
